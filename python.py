556e6a8acadc99bfc1f8d7b0cecec1ec5b0dbaac
Gustavo Gomes <Gustavoluc50@gmail.com>
2026-05-14 11:55:20 -0300

    Add os exercicicos da aula de matrizes

---
+++ python.py
@@ -0,0 +1,36 @@
+# matriz = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# print('Forma 1:')
+# print(matriz)
+
+# print('\nForma 2:')
+# for linha in matriz:
+#     print(linha)
+
+# print('\nForma 3:')
+# for i in range(len(matriz)):
+#     for j in range(len(matriz[i])):
+#         print(matriz[i][j], end=' ')
+#     print()
+#########################################################
+# matriz = [0][0] = 20
+# matriz = [1][2] = 15
+# matriz = [2][1] = 19
+
+# for linha in matriz:
+#     print(linha)
+#########################################################
+# soma = matriz[0][0] + matriz [1][0]
+# sub = matriz [2][2] - matriz [2][1]
+# multi = matriz[0][1] * matriz[2][0]
+# div = matriz[1][0] / matriz[0][2]
+
+# print(soma)
+# print(sub)
+# print(multi)
+# print(div)
+###########################################################
+
